5979320e94a9ce6ee1ee8cf173c57509ebe8b965
4n12i <an12inakamu12a@gmail.com>
2024-02-19 11:53:51 +0900

    Add options

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # dgen
+## Usage
+
+```
+Generate image with DALL·E and prnit it
+
+Usage: dgen [OPTIONS] <PROMPT>
+
+Arguments:
+  <PROMPT>  Prompt to pass to DALL·E
+
+Options:
+  -a, --ascii    Convert an image to ASCII art
+  -h, --help     Print help
+  -V, --version  Print version
+```
+
+```sh
+$ cd dgen
+$ cargo build
+$ ./target/debug/dgen "prompt"
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+
+/// Generate image with DALL·E and prnit it
+#[derive(Parser)]
+#[command(version, about, long_about = None)] // Read from Cargo.toml
+struct Args {
+    /// Prompt to pass to DALL·E
+    prompt: String,
+
+    /// Convert an image to ASCII art
+    #[arg(short, long)]
+    ascii: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    println!("{}", args.prompt);
+    if args.ascii {
+        println!("Convert an image to ASCII art and output");
+    } else {
+        println!("Output an image as it is");
+    }
+
+    Ok(())
 }
