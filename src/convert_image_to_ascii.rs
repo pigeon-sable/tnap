81cc8263855a03a32202d44f53078d0f0602be2c
shuheykoyama <shuheykoyama@gmail.com>
2024-02-20 19:20:51 +0900

    feat: ✨ allow width to be specified

--- src/convert_image_to_ascii.rs
+++ src/convert_image_to_ascii.rs
@@ -1,9 +1,10 @@
 use anyhow::{anyhow, Result};
 use artem::{config::ConfigBuilder, convert};
 use image::io::Reader as ImageReader;
+use std::num::NonZeroU32;
 use std::path::Path;
 
-pub fn convert_image_to_ascii(image_path: &str) -> Result<()> {
+pub fn convert_image_to_ascii(image_path: &str, width: Option<u32>) -> Result<()> {
     // Open the image file
     let img = ImageReader::open(Path::new(image_path))
         .map_err(|e| anyhow!("Failed to open image: {}", e))?
@@ -11,14 +12,16 @@ pub fn convert_image_to_ascii(image_path: &str) -> Result<()> {
         .map_err(|e| anyhow!("Failed to decode image: {}", e))?;
 
     // // Conversion Config
-    // let mut config_builder = ConfigBuilder::default();
-    // if let Some(w) = width {
-    //     config_builder = config_builder.width(w);
-    // }
-    // let config = config_builder.colored(colored).build();
+    let mut config_builder = ConfigBuilder::new();
+    if let Some(w) = width {
+        if let Some(target_width) = NonZeroU32::new(w) {
+            config_builder.target_size(target_width);
+        }
+    }
+    let config = config_builder.build();
 
     // Convert image to ASCII
-    let ascii_art = convert(img, &artem::config::ConfigBuilder::new().build());
+    let ascii_art = convert(img, &config);
 
     println!("{}", ascii_art);
 
